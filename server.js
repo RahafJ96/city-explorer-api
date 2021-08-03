accbbb05873d1a316e3269cdea99c128d6c02384
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-08-03 15:49:03 +0300

    lab07 solved

--- server.js
+++ server.js
@@ -1,57 +1,41 @@
 'use strict';
 
-const express = require('express'); // import express
-const server = express();
+require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
-// require('dotenv').config();
+const weather = require('./data/weather.json');
 
-//import the weather data
-const weatherData = require('./data/weather.json');
-server.use(cors()); // the server can take any req from any client
+const server = express();
+server.use(cors());
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 
+//localhost:3001/weather?searchQuery=amman
+server.get('/weather', getWeather);
+server.use('*', (request, response) => response.status(404).send('page not found'));
 
-// http://localhost:3001/ (/ === root route)
-server.get('/',(req,res) => {
-    res.send('Hi From the root route');
-})
+function getWeather(request, response) {
+  let searchQuery = request.query.searchQuery;
+  const city = weather.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
+  if(city != undefined)
+  {
+    const weatherArray = city.data.map(day => new Forecast(day));
+    response.status(200).send(weatherArray);
+  }
+  else
+  {
+    errorHandler(response);
+  }
+}
 
-// http://localhost:3001/test (/test === route)
-server.get('/test',(request,response) => {
-    let str = 'Hello From the server side';
-    response.send(str);
-})
+function errorHandler(response) {
+  response.status(500).send('something went wrong');
+}
+  
 
-// http://localhost:3001/getWeatherStatus (/getWeatherStatus === route)
-server.get('/getWeatherStatus',(req,res) => {
-    console.log(weatherData);
-    let getData = weatherData.map(item => {
-        return item.city_name
-    })
-    console.log(getData);
-    res.status(200).send(getData);
-})
+function Forecast(day) {
+  this.date = day.valid_date
+  this.description = day.weather.description
+}
 
-
-// http://localhost:3001/getWeatherInfo?city_name=Amman (/getWeatherInfo === route)
-server.get('/getWeatherInfo', (req,res) => {
-    console.log(req.query);
-    const cityName = req.query.city_name;
-    console.log(cityName);
-    let weatherItem = weatherData.find(item => {
-        if(item.city_name == cityName)
-        return item;
-    })
-    console.log(weatherItem);
-    res.send(weatherItem);
-})
-
-server.get('*', (req,res) => {
-    res.status(404).send('Page not Found');
-})
-
-
-server.listen(PORT,() => {
-    console.log(`I am listening on PORT = ${PORT}`);
-})
+server.listen(PORT, () => console.log(`I'm listening on ${PORT}`))
