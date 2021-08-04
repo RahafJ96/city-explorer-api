4a9a96f444af9b1b7f26cdda273ee4d95b2b7748
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-08-05 02:27:51 +0300

    lab09 done

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 const axios = require('axios');
 server.use(cors());
 const PORT = process.env.PORT;
-const Weather = require('./Weather')
-const movies = require('./movies')
+const Weather = require('./Weather');
+const Movie = require('./Movie')
 //Weather
 
 //http://localhost:3010/getWeather?lat=31.95&lon=35.91&cityName=Amman
@@ -55,55 +55,55 @@ server.get('/getWeather', Weather.handelWeather);
 //MOVIES
 //https://api.themoviedb.org/3/movie/550?api_key=41875812c2b323e366b30029131d151b
 //http://localhost:3010/movies?city=Amman
-server.get('/movies', movies.handelMovies);
+server.get('/movies', Movie.handelMovies);
 
-async function handelMovies(req, res) {
-    const city = req.query.city
-    const URLMovie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_KEY}&query=${city}`
+// async function handelMovies(req, res) {
+//     const city = req.query.city
+//     const URLMovie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_KEY}&query=${city}`
 
-    axios
-        .get(URLMovie)
-        .then(movieStatus => {
-            let moviesArray = movieStatus.data.results
-            res.send(moviesForObject(moviesArray));
-        })
-        .catch(err => {
-            res.send(err);
-        })
-}
-
-
-const moviesForObject = (moviesObj) => {
-
-    const forMoviesObj = [];
-    moviesObj.map(element => {
-
-    const title = element.title
-    const overview = element.overview
-    const vote_average = element.vote_average
-    const vote_count = element.vote_count
-    const poster_path = process.env.IMG_URL+element.poster_path
-    const popularity = element.popularity
-    const release_date = element.release_date
-
-        forMoviesObj.push(new Movies(title,overview,vote_average,vote_count,poster_path,popularity,release_date));
-
-        console.log(forMoviesObj);
-    });
-    return forMoviesObj;
-}
+//     axios
+//         .get(URLMovie)
+//         .then(movieStatus => {
+//             let moviesArray = movieStatus.data.results
+//             res.send(moviesForObject(moviesArray));
+//         })
+//         .catch(err => {
+//             res.send(err);
+//         })
+// }
 
 
-class Movies {
-    constructor(title,overview,vote_average,vote_count,poster_path,popularity,release_date) {
-    this.title = title
-    this.overview = overview
-    this.vote_average = vote_average
-    this.vote_count = vote_count
-    this.poster_path = poster_path
-    this.popularity = popularity
-    this.release_date = release_date
-    }
-}
+// const moviesForObject = (moviesObj) => {
+
+//     const forMoviesObj = [];
+//     moviesObj.map(element => {
+
+//     const title = element.title
+//     const overview = element.overview
+//     const vote_average = element.vote_average
+//     const vote_count = element.vote_count
+//     const poster_path = process.env.IMG_URL+element.poster_path
+//     const popularity = element.popularity
+//     const release_date = element.release_date
+
+//         forMoviesObj.push(new Movies(title,overview,vote_average,vote_count,poster_path,popularity,release_date));
+
+//         console.log(forMoviesObj);
+//     });
+//     return forMoviesObj;
+// }
+
+
+// class Movies {
+//     constructor(title,overview,vote_average,vote_count,poster_path,popularity,release_date) {
+//     this.title = title
+//     this.overview = overview
+//     this.vote_average = vote_average
+//     this.vote_count = vote_count
+//     this.poster_path = poster_path
+//     this.popularity = popularity
+//     this.release_date = release_date
+//     }
+// }
 
 server.listen(PORT, () => {console.log(` I'm Listening to port number ${PORT} `)})
